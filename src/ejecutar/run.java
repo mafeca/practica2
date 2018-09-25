cabcdcd387f5cba4b722c58bde5c8f7884c6e3cb
mafeca <43571141+mafeca@users.noreply.github.com>
2018-09-25 12:31:38 +0200

    Update run.java

--- src/ejecutar/run.java
+++ src/ejecutar/run.java
@@ -15,5 +15,6 @@ public class run {
     public static void main(String[] args) {
         System.out.println("Hola Manolo");
         JOptionPane.showMessageDialog(null, "Hola Manolo");
+        JOptionPane.showMessageDialog(null, "Esto es fantastico");
      }
 }
