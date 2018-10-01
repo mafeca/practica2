f9267eca955f2cbf337790ffe21c87466253fb47
mafeca <43571141+mafeca@users.noreply.github.com>
2018-10-01 09:29:07 +0200

    Add files via upload

---
+++ clase1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba1;
+
+/**
+ *
+ * @author mfc
+ */
+public class clase1 {
+    public static void main(String[] args){
+        //creamos la variable integer con el valor 13
+        int var1 = 13;
+        System.out.println("Manuel Fernandez carrillo");
+        System.out.println("El valor de la variable es :"+var1);
+    }
+}
